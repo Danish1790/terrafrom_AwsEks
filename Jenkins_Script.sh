82817a6eb50db2b0a26fd7cfdddd9c5191f30759
danish1790 <danish.asif.7190@gmail.com>
2023-11-06 15:04:11 -0800

    git path

--- Jenkins_Script.sh
+++ Jenkins_Script.sh
@@ -7,7 +7,8 @@ export TF_VAR_vpc_id=$VPC_ID
 export TF_VAR_cluster_name=$CLUSTER_NAME
 
 #goint to the terraform path
-cd ${WORKSPACE}/DevOps_Project_3/Terraform
+git clone https://github.com/Danish1790/terrafrom_AwsEks.git
+cd ${WORKSPACE}/terrafrom_AwsEks/Terraform
 
 #replacing the cluster name field in backend.tf file
 sed -i "s/ngg_cluster_name/$CLUSTER_NAME/g" backend.tf
